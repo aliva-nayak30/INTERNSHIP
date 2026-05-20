cab80a8038cab4555b00890a29d2f593c0a66ced
aliva-nayak30 <aliva.nayak2022@gmail.com>
2026-05-20 21:19:57 +0530

    Create Swap.java

---
+++ Variable/src/com/variable/Swap.java
@@ -0,0 +1,20 @@
+package com.variable;
+
+public class Swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		        int a = 3;
+		        int b = 5;
+
+		        a = a + b;
+		        b = a - b;
+		        a = a - b;
+
+		        System.out.println(a);
+		        System.out.println(b);
+		    
+
+	}
+
+}
