4bdac7e891f1d53e3cfe9f223804194ddb21b4fb
aliva-nayak30 <aliva.nayak2022@gmail.com>
2026-05-20 21:05:42 +0530

    Create Datatype.java

---
+++ Variable/src/com/variable/Datatype.java
@@ -0,0 +1,27 @@
+package com.variable;
+
+public class Datatype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	        byte b = 127;
+	        short s = 32767;
+	        int i = 9898;
+	        long l = 8637255136L;
+	        float f = 3.14f;
+	        double d = 123.456;
+	        char c = 'J';
+	        boolean o= true;
+
+	        System.out.println(b);
+	        System.out.println(s);
+	        System.out.println(i);
+	        System.out.println(l);
+	        System.out.println(f);
+	        System.out.println(d);
+	        System.out.println(c);
+	        System.out.println(o);
+
+	}
+
+}
