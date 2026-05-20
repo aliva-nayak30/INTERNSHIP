53154237c264cf33cec050dc3a71961d79d8aebd
aliva-nayak30 <aliva.nayak2022@gmail.com>
2026-05-20 21:12:54 +0530

    Create Range.java

---
+++ Variable/src/com/variable/Range.java
@@ -0,0 +1,32 @@
+package com.variable;
+
+public class Range {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    System.out.println("byte size = " + Byte.SIZE + " bits");
+        System.out.println("byte range = " + Byte.MIN_VALUE + " to " + Byte.MAX_VALUE);
+
+        System.out.println("short size = " + Short.SIZE + " bits");
+        System.out.println("short range = " + Short.MIN_VALUE + " to " + Short.MAX_VALUE);
+
+        System.out.println("int size = " + Integer.SIZE + " bits");
+        System.out.println("int range = " + Integer.MIN_VALUE + " to " + Integer.MAX_VALUE);
+
+        System.out.println("long size = " + Long.SIZE + " bits");
+        System.out.println("long range = " + Long.MIN_VALUE + " to " + Long.MAX_VALUE);
+
+        System.out.println("float size = " + Float.SIZE + " bits");
+        System.out.println("float range = " + Float.MIN_VALUE + " to " + Float.MAX_VALUE);
+
+        System.out.println("double size = " + Double.SIZE + " bits");
+        System.out.println("double range = " + Double.MIN_VALUE + " to " + Double.MAX_VALUE);
+
+        System.out.println("char size = " + Character.SIZE + " bits");
+        System.out.println("char range = " + (int) Character.MIN_VALUE + " to " + (int) Character.MAX_VALUE);
+
+        System.out.println("boolean = true or false");
+
+	}
+
+}
