df923f3a8743af7f2e363c5609572af75669da77
aliva-nayak30 <aliva.nayak2022@gmail.com>
2026-05-20 20:50:35 +0530

    Create Variable.java
    Removed Basic project

---
+++ Variable/src/com/variable/Variable.java
@@ -0,0 +1,27 @@
+package com.variable;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		float b=20.5f;
+		double c=12.456;
+		char d='A';
+		boolean r=true;
+		short g=500;
+		long u=900000000L;
+		
+		System.out.println(a);
+		System.out.print(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(r);
+		System.out.println(g);
+		System.out.println(u);
+		
+		
+
+	}
+
+}
