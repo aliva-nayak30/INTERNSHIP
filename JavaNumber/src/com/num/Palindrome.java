fc580bd48c08925aaf4b2b86bd79609be4c6fb08
aliva-nayak30 <aliva.nayak2022@gmail.com>
2026-05-25 18:01:49 +0530

    Create Palindrome.java

---
+++ JavaNumber/src/com/num/Palindrome.java
@@ -0,0 +1,28 @@
+package com.num;
+import java.util.Scanner;
+public class Palindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		        Scanner sc = new Scanner(System.in);
+		        System.out.print("Enter a number: ");
+		        int num = sc.nextInt();
+		        int original = num;
+		        int reverse = 0;
+
+		        while(num != 0) {
+		            int digit = num % 10;
+		            reverse = reverse * 10 + digit;
+		            num = num / 10;
+		        }
+
+		        if(original == reverse) {
+		            System.out.println("Palindrome Number");
+		        } else {
+		            System.out.println("Not a Palindrome Number");
+		        }
+
+		        sc.close();
+		    }
+		}
+
