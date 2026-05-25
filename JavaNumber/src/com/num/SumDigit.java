8073958366332ec05ddecadee0af531f07805949
aliva-nayak30 <aliva.nayak2022@gmail.com>
2026-05-25 17:57:22 +0530

    Create SumDigit.java

---
+++ JavaNumber/src/com/num/SumDigit.java
@@ -0,0 +1,22 @@
+package com.num;
+import java.util.Scanner;
+public class SumDigit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		        Scanner sc = new Scanner(System.in);
+		        System.out.print("Enter a number: ");
+		        int num = sc.nextInt();
+		        int sum = 0;
+		        while(num != 0) {
+		            int digit = num % 10;
+		            sum = sum + digit;
+		            num = num / 10;
+		        }
+
+		        System.out.println("Sum of digits = " + sum);
+
+		        sc.close();
+		    }
+
+}
