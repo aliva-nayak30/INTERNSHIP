2a0f976b557ad30725422d0e994508b2e143151c
aliva-nayak30 <aliva.nayak2022@gmail.com>
2026-05-25 19:41:56 +0530

    Create EvenDigitSum.java

---
+++ Array/src/com/arr/EvenDigitSum.java
@@ -0,0 +1,31 @@
+package com.arr;
+import java.util.Scanner;
+public class EvenDigitSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		        Scanner sc = new Scanner(System.in);
+		        System.out.print("Enter size of array: ");
+		        int n = sc.nextInt();
+		        int arr[] = new int[n];
+		        System.out.println("Enter array elements:");
+
+		        for(int i = 0; i < n; i++) {
+		            arr[i] = sc.nextInt();
+		        }
+
+		        int sum = 0;
+
+		        for(int i = 0; i < n; i++) {
+		            if(arr[i] % 2 == 0) {
+		                sum = sum + arr[i];
+		            }
+		        }
+
+		        System.out.println("Sum of even elements = " + sum);
+
+		        sc.close();
+
+	}
+
+}
