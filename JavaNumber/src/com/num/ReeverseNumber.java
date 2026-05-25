1d4945e13144cd670a8d3d9c0e739b8858dfc66b
aliva-nayak30 <aliva.nayak2022@gmail.com>
2026-05-25 17:59:21 +0530

    Create ReeverseNumber.java

---
+++ JavaNumber/src/com/num/ReeverseNumber.java
@@ -0,0 +1,23 @@
+package com.num;
+import java.util.Scanner;
+public class ReeverseNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		        Scanner sc = new Scanner(System.in);
+		        System.out.print("Enter a number: ");
+		        int num = sc.nextInt();
+		        int reverse = 0;
+		        while(num != 0) {
+		            int digit = num % 10;
+		            reverse = reverse * 10 + digit;
+		            num = num / 10;
+		        }
+
+		        System.out.println("Reversed number = " + reverse);
+
+		        sc.close();
+		    }
+		}
+
+	
