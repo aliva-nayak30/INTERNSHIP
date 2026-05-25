75f29524bf2a2b3938f1ab497aa71a844282b32f
aliva-nayak30 <aliva.nayak2022@gmail.com>
2026-05-25 17:38:13 +0530

    Create EvenOdd.java

---
+++ JavaNumber/src/com/num/EvenOdd.java
@@ -0,0 +1,21 @@
+package com.num;
+import java.util.Scanner;
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub   
+		        Scanner sc = new Scanner(System.in);
+
+		        System.out.print("Enter a number: ");
+		        int num = sc.nextInt();
+
+		        if(num % 2 == 0) {
+		            System.out.println("Even Number");
+		        } else {
+		            System.out.println("Odd Number");
+		        }
+
+		        sc.close();
+}
+
+}
