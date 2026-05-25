b0a601ad924880481286a5a8a04f83ad35f91c73
aliva-nayak30 <aliva.nayak2022@gmail.com>
2026-05-25 18:48:33 +0530

    Create Factorial.java

---
+++ JavaNumber/src/com/num/Factorial.java
@@ -0,0 +1,20 @@
+package com.num;
+import java.util.Scanner;
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		        Scanner sc = new Scanner(System.in);
+		        System.out.print("Enter a number: ");
+		        int num = sc.nextInt();
+		        int fact = 1;
+		        for(int i = 1; i <= num; i++) {
+		            fact = fact * i;
+		        }
+
+		        System.out.println("Factorial = " + fact);
+
+		        sc.close();
+		    }
+
+}
