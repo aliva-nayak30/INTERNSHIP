9affbbe2b332b11ed1b0e5713482cad678c11f55
aliva-nayak30 <aliva.nayak2022@gmail.com>
2026-05-29 12:36:51 +0530

    Update SumArray.java

--- Array/src/com/arr/SumArray.java
+++ Array/src/com/arr/SumArray.java
@@ -9,18 +9,14 @@ public class SumArray {
 
 		        System.out.print("Enter size of array: ");
 		        n = sc.nextInt();
-
 		        int arr[] = new int[n];
-
 		        System.out.println("Enter array elements:");
-
 		        for(i = 0; i < n; i++) {
 		            arr[i] = sc.nextInt();
 		        }
 		        for(i = 0; i < n; i++) {
 		            sum = sum + arr[i];
 		        }
-
 		        System.out.println("Sum of array elements is: " + sum);
 
 		        sc.close();
