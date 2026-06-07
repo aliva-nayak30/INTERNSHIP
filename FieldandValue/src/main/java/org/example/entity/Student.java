55b58fe1c93e26a7320ac9de3d99e8b7eead70e0
aliva-nayak30 <aliva.nayak2022@gmail.com>
2026-06-07 20:39:44 +0530

    Create Student.java

---
+++ FieldandValue/src/main/java/org/example/entity/Student.java
@@ -0,0 +1,39 @@
+package org.example.entity;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;  // ← I added this import
+
+@Component  // ← I added this annotation. This fixes NoSuchBeanDefinitionException
+public class Student {
+    private String name;
+    private String branch;
+    private String adress;
+    private String hobby;
+
+    @Value("${student.name}")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Value("${student.branch}")
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    @Value("${student.adress}")
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    @Value("${student.hobby}")
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public void display(){
+        System.out.println("Name of Student  :" + name);
+        System.out.println("Name of the Branch :" + branch);
+        System.out.println("Adress :" + adress);
+        System.out.println("Hobby  :" + hobby);
+    }
+}
